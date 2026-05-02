2ff79b046b45fc0f90c57caaacfcd557fe4c4499
Zachary Levesque <zlevesque93@gmail.com>
2026-05-02 13:48:17 -0400

    Update router.py

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from app.api.routes.health import router as health_router
 from app.api.routes.market import router as market_router
 from app.api.routes.recommendation import router as recommendation_router
+from app.api.routes.system import router as system_router
 
 
 api_router = APIRouter()
 api_router.include_router(health_router, tags=["health"])
 api_router.include_router(market_router, tags=["market"])
 api_router.include_router(recommendation_router, tags=["recommendation"])
+api_router.include_router(system_router, tags=["system"])
